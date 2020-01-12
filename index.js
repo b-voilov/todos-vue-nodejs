790bf779d20f0ba8eb3e55d6ab20873df77b9a30
Bogdan <voylovbogdan@gmail.com>
2020-01-12 14:13:06 +0200

    fixed bug when date filters dont marked

--- index.js
+++ index.js
@@ -6,7 +6,4 @@ var oneDay = 86400000;
 var app = connect()
     .use(gzipStatic(__dirname + '/dist'));
 
-connect()
-    .use(gzipStatic(__dirname + '/dist', { maxAge: oneDay }));
-
 http.createServer(app).listen(process.env.PORT || 4000);
